44f3fb9d83e0049af2c0a9d36ca5350530e456d6
esiammd <esiammd@hotmail.com>
2024-10-30 11:29:46 +0100

    feat(utils): add hexToRgba function

---
+++ src/utils/hexToRgba.ts
@@ -0,0 +1,9 @@
+function hexToRgba(hex: string, alpha: number) {
+  hex = hex.replace(/^#/, '');
+  const r = parseInt(hex.slice(0, 2), 16);
+  const g = parseInt(hex.slice(2, 4), 16);
+  const b = parseInt(hex.slice(4, 6), 16);
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+}
+
+export default hexToRgba;
