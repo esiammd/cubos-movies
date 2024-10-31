912ee5b0ee2d72776e47ba7f1f83d628d868ac09
esiammd <esiammd@hotmail.com>
2024-10-31 14:44:14 +0100

    chore(lib): add environment variable

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,15 @@
 import axios from 'axios';
 
+const apiKey = import.meta.env.VITE_KEY_API_TMDB;
+
 export const api = axios.create({
-  baseURL: 'https://api.themoviedb.org/3/discover',
+  baseURL: 'https://api.themoviedb.org/3/',
+});
+
+api.interceptors.request.use(config => {
+  config.params = {
+    ...config.params,
+    api_key: apiKey,
+  };
+  return config;
 });
